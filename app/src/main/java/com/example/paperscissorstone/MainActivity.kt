35e1d5978ad748e3ec03fc28dd12df7d203cb805
李宜雯 <liy721511@gmail.com>
2020-02-26 16:46:06 +0800

    Second Commit

--- app/src/main/java/com/example/paperscissorstone/MainActivity.kt
+++ app/src/main/java/com/example/paperscissorstone/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.paperscissorstone
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,85 +16,103 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_mora.setOnClickListener {
-            if (ed_name.length()<1)
-                tv_text.text = "請輸入玩家姓名"
-            else{
-                tv_name.text = "姓名\n${ed_name.text}"
-                tv_mmora.text=
-                    "我方出拳\n${
-                    
-                    
-                    when{
-                        (btn_scissor.isChecked) -> "剪刀"
-                        (btn_stone.isChecked) -> "石頭"
-                        else  -> "布"
-                  
-                    }
-                    
-                    }"
+            if (btn_paper.isChecked == false &&
+                btn_stone.isChecked == false &&
+                btn_scissor.isChecked ==false
 
-                val computer =(Math.random()*3).toInt()
+                    )
 
-                tv_mmora.text= "電腦出拳\n${
+                Toast.makeText(this@MainActivity,"請選擇你的出拳",Toast.LENGTH_SHORT).show()
+
+            else {
 
 
-                when{
-                    (computer==0) -> "剪刀"
-                    (computer==1) -> "石頭"
-                    else  -> "布"
-
-                  }
-
-                }"
+                val computer = (Math.random() * 3).toInt()
 
 
-                when{
+                // 0 是剪刀  1 是石頭  2是布
+
+                set_you()
+
+                set_computer(computer)
 
 
-                    btn_scissor .isChecked && computer == 2 ||
-                    btn_stone.isChecked && computer ==0||
-                    btn_paper.isChecked && computer== 1 ->{
 
-                       tv_winner.text ="\n${ed_name.text}"
-                       tv_text.text  ="恭喜你獲勝了！！"
+                when {
+
+                    btn_scissor.isChecked && computer == 2 ||
+                            btn_stone.isChecked && computer == 0 ||
+                            btn_paper.isChecked && computer == 1 -> {
+
+
+                        result_textview.text = "恭喜你獲勝了！！"
 
 
                     }
 
                     btn_scissor.isChecked && computer == 1 ||
 
-                    btn_stone.isChecked &&  computer == 2 ||
+                            btn_stone.isChecked && computer == 2 ||
 
-                     btn_paper.isChecked && computer ==0  ->{
+                            btn_paper.isChecked && computer == 0 -> {
 
-                        tv_winner.text = "勝利者\n電腦"
-                        tv_text.text="可惜，電腦獲勝了"
+
+                        result_textview.text = "可惜，電腦獲勝了"
 
                     }
 
-                   else ->{
-                       tv_winner.text="勝利者\n平手"
-                       tv_text.text="平局,再試一次"
+                    btn_scissor.isChecked && computer == 0 ||
 
+                            btn_stone.isChecked && computer == 1 ||
 
+                            btn_paper.isChecked && computer == 2 -> {
 
-                   }
+                        result_textview.text = "平局,再試一次"
 
-
-
+                    }
 
 
                 }
+
+
             }
+        }
+
+
+    }
+
+    fun set_you(){
+
+        when{
+
+            btn_scissor.isChecked -> you_textview.text = "剪刀"
+
+            btn_stone.isChecked   ->  you_textview.text =  "石頭"
+
+            btn_paper.isChecked  ->  you_textview.text =  "布"
+
 
 
         }
 
 
+    }
+
+    fun set_computer(computer:Int){
+
+        when{
+
+            computer == 1 -> computer_textView.text = "石頭"
+
+            computer == 0 -> computer_textView.text = "剪刀"
+
+            computer == 2 -> computer_textView.text = "布"
 
 
 
 
+
+        }
 
 
 
